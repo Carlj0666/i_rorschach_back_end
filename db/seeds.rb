2ab7b8ae4c12e53f79121a93f044e103bf1526d6
Carlj0666 <carlj1881@yahoo.com>
2021-06-24 18:45:40 -0700

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+images = Inkblot.create(
+    [
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_01.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_02.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_03.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_04.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_05.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_06.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_07.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_08.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_09.jpg'},
+        {link: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_10.jpg'},
+    ])
+
+# comment = Comment.create(verbiage: "Cool")
