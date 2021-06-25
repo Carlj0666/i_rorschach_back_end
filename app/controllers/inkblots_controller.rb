7d7e8ea6d328c8235efd954a1a36bcbef89eb0e4
Carlj0666 <carlj1881@yahoo.com>
2021-06-24 18:54:47 -0700

    practice sending json to the server

--- app/controllers/inkblots_controller.rb
+++ app/controllers/inkblots_controller.rb
@@ -1,2 +1,8 @@
 class InkblotsController < ApplicationController
+    def index
+    inkblots = Inkblot.all
+    render json:inkblots
+    # May need below to render in REACT app instead of line 4
+    # render json: inkblts.to_json(except: [:created_at, :updated_at])
+    end
 end
